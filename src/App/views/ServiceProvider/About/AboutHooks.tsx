b90fbb3f1eb659e65cf7f5a5c902c915929a2aee
Eric Yager <eric@northcountryengineer.com>
2023-05-22 13:57:44 -0400

    cleaning up abouthooks.tsx

--- src/App/views/ServiceProvider/About/AboutHooks.tsx
+++ src/App/views/ServiceProvider/About/AboutHooks.tsx
@@ -1,41 +1,25 @@
 import { useEffect, useState } from 'react';
-import { getAppClientAccessToken, useAuthentication } from '../../Authentication';
+import { getAppClientAccessToken } from '../../Authentication';
 import { API, Auth, graphqlOperation } from 'aws-amplify';
 import { getUser } from '../../../../graphql/queries';
 import { ServiceProviderSignUpProps, ServiceProviderSignupInitialState } from './AboutTypes.d';
 
 
 export function ServiceProviderSignupHooks() {
-    const [address, setAddress] = useState('')
-    const [activeStep, setActiveStep] = useState(0)
-    const [companyName, setCompanyName] = useState('')
-    const [confPassword, setConfPassword] = useState('')
-    const [email, setEmail] = useState('')
-    const [errorMessage, setErrorMessage] = useState('')
-    const [firstName, setFirstName] = useState('')
-    const [loadingComplete, setLoadingComplete] = useState(false)
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
-    const [isComplete, setIsComplete] = useState(false)
-    const [isConfPasswordFocused, setIsConfPasswordFocused] = useState(false)
-    const [lastName, setLastName] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordsMatch, setPasswordsMatch] = useState(true)
-    const [phone, setPhone] = useState('')
-    const [skills, setSkills] = useState('')
-    const [town, setTown] = useState('')
-    const [userID, setUserID] = useState('')
-    const [verificationCodeSent, setVerificationCodeSent] = useState(false)
-    const [verificationModalOpen, setVerificationModalOpen] = useState(false)
-    const [verificationSuccess, setVerificationSuccess] = useState(false)
-    const test = useState("test")
 
     const [serviceProviderSignupAttributes, setServiceProviderSignupAttributes] = useState<ServiceProviderSignUpProps>(ServiceProviderSignupInitialState)
 
-    const updateServiceProviderFormState = (updates: Partial<ServiceProviderSignUpProps>) => {
-        console.log({...serviceProviderSignupAttributes, ...updates})
-        setServiceProviderSignupAttributes({...serviceProviderSignupAttributes, ...updates});
-        console.log(serviceProviderSignupAttributes)
-    };
+    const checkUserObjectExists = async (userId) => {
+        try {
+          const query = graphqlOperation(getUser, { id: userId });
+          const data: any = await API.graphql(query);
+          return !!data.getUser;
+        } catch (error) {
+          console.error(error);
+          return false;
+        }
+      };
+
 
     const SetUserAuthenticationAttributes = async () => {
         const authenticationStatus = getAppClientAccessToken();
@@ -73,17 +57,6 @@ export function ServiceProviderSignupHooks() {
           return false;
         }
         return false;
-      };
-      
-      const checkUserObjectExists = async (userId) => {
-        try {
-          const query = graphqlOperation(getUser, { id: userId });
-          const data: any = await API.graphql(query);
-          return !!data.getUser;
-        } catch (error) {
-          console.error(error);
-          return false;
-        }
       };
       
 
