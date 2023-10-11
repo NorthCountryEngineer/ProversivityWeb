a82323959f628c9070522aea17522b55a6e8a305
Eric Yager <eric@northcountryengineer.com>
2023-10-11 05:36:47 -0400

    continuing bug hunt

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
+    baseUrl: "http://localhost:3000",
   },
   env: {
       "username": "eric@northcountryengineer.com", 
@@ -12,4 +11,5 @@ export default defineConfig({
       "userPoolId": "us-east-1_7d0kzod7w", 
       "clientId": "2rjh78vei45uc9seknfqkljsbj"
   },
+  reporter: "mochawesome",
 });
