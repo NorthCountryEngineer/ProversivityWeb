a49b6162a874eb8964c6f61be78916bdc53a2c41
Eric Yager <eric@northcountryengineer.com>
2023-10-11 06:25:19 -0400

    added test for user authentication

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,17 @@ export default defineConfig({
     baseUrl: "http://localhost:3000",
   },
   env: {
-      "username": "eric@northcountryengineer.com", 
+      "username": "eric.p.yager@gmail.com", 
       "password": "$ocAdmin12", 
       "userPoolId": "us-east-1_7d0kzod7w", 
       "clientId": "2rjh78vei45uc9seknfqkljsbj"
   },
   reporter: "mochawesome",
+  reporterOptions: {
+    "reportDir": "cypress/report/mochawesome-report",
+    "overwrite": false,
+    "html": false,
+    "json": true,
+    "timestamp": "mmddyyyy_HHMMss"
+  }
 });
--- cypress/e2e/Auth.cy.ts
+++ cypress/e2e/Auth.cy.ts
@@ -7,3 +7,24 @@ describe('Unauthorized Access Test', () => {
     cy.contains('Sign In').should('exist'); // Check for the text 'Sign In'
   });
 });
+
+
+describe('Unauthorized Access Test', () => {
+  it('Renders Amplify useAuthentication() view when user is not authenticated', () => {
+    // Visit the page you want to test
+    cy.visit('localhost:3000/OneOnOneHelper');
+
+    // Fill in the username field
+    cy.get('input[name="username"]').type(Cypress.env('username'));
+
+    // Fill in the password field
+    cy.get('input[name="password"]').type(Cypress.env('password'));
+
+    // Click the "Sign in" button
+    cy.get('button[type="submit"]').click();
+
+    // Now, wait for the "You are not signed up yet" text to appear
+    cy.contains('You are not signed up yet').should('exist');
+  });
+});
+
